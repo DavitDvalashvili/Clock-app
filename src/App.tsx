992f0fd30f2aec456d80f8cdf0ff8dc48cee4d7f
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-05-30 23:40:36 +0400

    feat: add quotecontainer component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+//import { useEffect, useState} from 'react';
+import './App.css';
+//import axios from 'axios';
+import QuoteContainer from './components/QuoteContainer';
+
+
+function App() {
+
+
+  return (
+    <QuoteContainer/>
+  );
+}
+
+export default App;
---
+++ src/components/QuoteContainer.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+
+const QuoteContainer = () => {
+  interface QuoteType {
+    content: string;
+    author: string;
+  }
+  
+
+  const [refreshCount, setRefreshCount] = useState<number>(0);  
+  const [Quote, setQuote] = useState<QuoteType | null>(null);
+
+  useEffect(() => {
+    const randomQuote = async () => {
+      const response = await axios.get(
+        "https://api.quotable.io/quotes/random?tags=technology,famous-quotes"
+      );
+      const data = await response.data;
+      setQuote(data[0]);
+      console.log(Quote)
+    };
+    randomQuote()
+  }, [refreshCount] );
+
+
+  return (
+    <div className="quote-container">
+      <div>
+      {Quote && <p>{Quote.content}</p>}
+      <img src="./assets/images/icon-refresh.svg" alt="refresh" onClick={() => {
+        setRefreshCount(refreshCount+1);
+      }}/>
+      </div>
+      {Quote && <h2>{Quote.author}</h2>}
+        
+    </div>
+  );
+};
+
+export default QuoteContainer;
